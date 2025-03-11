d4744fa89fb00c6d1f4ea61edf66b91502e9b1fd
PratikNichite <pnichite77@gmail.com>
2025-03-11 14:40:13 +0100

    test: refactor all tests

--- test/test_db_helper.py
+++ test/test_db_helper.py
@@ -1,17 +1,25 @@
 import pytest
 from backend import db_helper
+def test_database_connection():
+    with db_helper.get_db_cursor() as cursor:
+        assert cursor is not None
+        assert cursor._connection.is_connected()
 
-def test_db_connection():
-    try:
-        with db_helper.get_db_cursor() as cursor:
-            assert cursor is not None
-            assert cursor._connection.is_connected()
-    except Exception as e:
-        pytest.fail(f"Database connection failed: {e}")
-        
+def test_fetch_expenses_by_date():
+    date = "2025-03-10"
+    expenses = db_helper.fetch_expenses(date)
+    assert isinstance(expenses, list)
+    for expense in expenses:
+        assert "expense_date" in expense
+        assert expense["expense_date"] == date
 
-def test_fetch_expenses():
-    try:
-        assert len(db_helper.fetch_expenses("2024-08-01")) >= 1
-    except Exception as e:
-        pytest.fail(f"Fetch error: {e}")
+def test_create_expense():
+    date = "1600-01-01"
+    amount = 100
+    category = "Entertainment"
+    notes = "Movie"
+    
+    db_helper.create_expense(date, amount, category, notes)
+    
+    expenses = db_helper.fetch_expenses(date)
+    assert any(exp["amount"] == amount and exp["category"] == category and exp["notes"] == notes for exp in expenses)
