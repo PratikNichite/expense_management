f89facfda46117d5cd1e85789cee8bd6a40587a6
PratikNichite <pnichite77@gmail.com>
2025-03-11 14:20:56 +0100

    feat: add fetch expense test

--- test/test_db_helper.py
+++ test/test_db_helper.py
@@ -8,3 +8,13 @@ def test_db_connection():
             assert cursor._connection.is_connected()
     except Exception as e:
         pytest.fail(f"Database connection failed: {e}")
+        
+
+def test_fetch_expenses():
+    try:
+        with db_helper.get_db_cursor() as cursor:
+            query = "SELECT * FROM expenses WHERE expense_date=%s"
+            cursor.execute(query, ("2024-08-01",))
+            assert len(cursor.fetchall()) >= 1
+    except Exception as e:
+        pytest.fail(f"Fetch error: {e}")
